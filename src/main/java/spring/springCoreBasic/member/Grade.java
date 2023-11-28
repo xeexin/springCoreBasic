a63d1717ce3f48260a7174c073929ce7f175c36a
LIM HEE JIN <xeexin0331@gmail.com>
2023-11-28 15:58:30 +0900

    회원 도메인 개발

---
+++ src/main/java/spring/springCoreBasic/member/Grade.java
@@ -0,0 +1,4 @@
+package spring.springCoreBasic.member;
+
+public enum Grade {
+}
---
+++ src/main/java/spring/springCoreBasic/member/MemberService.java
@@ -0,0 +1,7 @@
+package spring.springCoreBasic.member;
+
+public interface MemberService {
+    void join(Member member);
+
+    Member findMember(Long memberId);
+}
