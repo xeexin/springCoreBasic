1033d2fc37d6f5d16ec533d16df3a1b9e22256be
LIM HEE JIN <xeexin0331@gmail.com>
2023-11-30 10:10:52 +0900

    새로운 할인 도메인 실행과 테스트

--- src/main/java/spring/springCoreBasic/OrderApp.java
+++ src/main/java/spring/springCoreBasic/OrderApp.java
@@ -14,6 +14,7 @@ public class OrderApp {
         OrderService orderService = new OrderServiceImpl();
 
         Long memberId=1L;
+
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
