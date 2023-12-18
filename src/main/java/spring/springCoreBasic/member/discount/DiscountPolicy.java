f2d0bc0fbca5700b8459936234e409affbd5cbda
LIM HEE JIN <xeexin0331@gmail.com>
2023-12-18 12:05:38 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점

--- src/main/java/spring/springCoreBasic/member/discount/DiscountPolicy.java
+++ src/main/java/spring/springCoreBasic/member/discount/DiscountPolicy.java
@@ -2,6 +2,7 @@ package spring.springCoreBasic.member.discount;
 
 import spring.springCoreBasic.member.Member;
 
+
 public interface DiscountPolicy {
     /**
      *
--- src/main/java/spring/springCoreBasic/member/discount/RateDiscountPolicy.java
+++ src/main/java/spring/springCoreBasic/member/discount/RateDiscountPolicy.java
@@ -16,3 +16,4 @@ public class RateDiscountPolicy implements DiscountPolicy {
 
     }
 }
+
