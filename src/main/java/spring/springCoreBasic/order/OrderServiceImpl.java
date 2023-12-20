af9706dd5f6fc42840ed8f6e3c80247ff644915f
LIM HEE JIN <xeexin0331@gmail.com>
2023-12-20 15:18:53 +0900

    오류 수정

--- src/main/java/spring/springCoreBasic/order/OrderServiceImpl.java
+++ src/main/java/spring/springCoreBasic/order/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository,@MainDiscountPolicy DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
