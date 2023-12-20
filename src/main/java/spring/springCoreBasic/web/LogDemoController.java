806e5ea2f335519606ed1465ae10e7656d038d26
LIM HEE JIN <xeexin0331@gmail.com>
2023-12-20 12:47:41 +0900

    스코프와 프록시

--- src/main/java/spring/springCoreBasic/web/LogDemoController.java
+++ src/main/java/spring/springCoreBasic/web/LogDemoController.java
@@ -13,14 +13,13 @@ import spring.springCoreBasic.common.MyLogger;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestUrl = request.getRequestURI().toString();
 
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestUrl);
 
         myLogger.log("controller test");
